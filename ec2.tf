d413a43e7a8a7beddc094174db86c9c93328c0e2
jwoutsourcing <ec2-user@ip-172-31-52-153.ec2.internal>
2019-04-08 11:35:34 +0000

    EC2 instances

---
+++ ec2.tf
@@ -0,0 +1,14 @@
+resrouce "aws_instance" "web" { 
+  ami = "${var.instance.default}"
+  count = "${length(split(",", lookup(var.azs, var.provider["region"])))}"
+  availability_zone = "${element(split(",", lookup(var.azs, var.provider["region"])), count.index)}" 
+  instance_type = "t2.micro"
+  subnet_id = "${element(aws_subnet.pub-net.*.id, count.index)}"
+  
+  tags = {
+    name = "Web Servers"
+    buildwith = "Terraform"
+  }
+
+}
+
