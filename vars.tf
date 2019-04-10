0653b434b626eeb7e9772231635c0feaf6fcd28f
jwoutsourcing <ec2-user@ip-172-31-52-153.ec2.internal>
2019-04-10 09:40:29 +0000

    Added terraform backend

--- vars.tf
+++ vars.tf
@@ -2,28 +2,40 @@ provider "aws" {
   shared_credentials_file = "/app/.aws/credentials"
 }
 
+terraform {
+  backend "s3" {
+    shared_credentials_file = "/app/.aws/credentials"
+    bucket                  = "steve-wood-tfstate"
+    key                     = "terraform/tfstate"
+    region                  = "us-east-1"
+  }
+}
+
 variable "provider" {
-    type = "map"
-    default = {
-        region     = "us-east-1"
-    }
+  type = "map"
+
+  default = {
+    region = "us-east-1"
+  }
 }
 
 variable "azs" {
-    type = "map"
-    default = {
-        "us-east-1"  = "us-east-1a,us-east-1b,us-east-1c"
-    }
+  type = "map"
+
+  default = {
+    "us-east-1" = "us-east-1a,us-east-1b,us-east-1c"
+  }
 }
 
 variable vpc {
-    type = "map"
-    default =  {
-    cidr_block            = "10.0.0.0/20"
-    subnet_bits           = "4"
-   }
+  type = "map"
+
+  default = {
+    cidr_block  = "10.99.0.0/20"
+    subnet_bits = "4"
+  }
 }
 
 variable ami {
-   default =  "ami-fbacaaec"
+  default = "ami-fbacaaec"
 }
