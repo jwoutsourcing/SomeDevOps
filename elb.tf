a4fcb4525e1e264b0713bd3515bbf6087a5a9a69
jwoutsourcing <ec2-user@ip-172-31-52-153.ec2.internal>
2019-04-10 09:44:18 +0000

    Merge

--- elb.tf
+++ elb.tf
@@ -1,10 +1,7 @@
 resource "aws_elb" "lb" {
   count              = "${length(split(",", lookup(var.azs, var.provider["region"])))}"
   availability_zones = ["${element(split(",", lookup(var.azs, var.provider["region"])), count.index)}"]
-<<<<<<< HEAD
-=======
   subnets            = ["${element(aws_subnet.priv-net.*.id, count.index)}"]
->>>>>>> f3fa6dd128c29b7b6086cd081909f7d4969869b0
   name               = "test-elb"
 
   listener {
@@ -12,8 +9,6 @@ resource "aws_elb" "lb" {
     instance_protocol = "http"
     lb_port           = "80"
     lb_protocol       = "http"
-<<<<<<< HEAD
-=======
   }
 
   listener {
@@ -22,7 +17,6 @@ resource "aws_elb" "lb" {
     lb_port            = 443
     lb_protocol        = "https"
     ssl_certificate_id = "arn:aws:acm:us-east-2:953116172017:certificate/7078f7a1-822e-4713-9d43-c61b5df041df"
->>>>>>> f3fa6dd128c29b7b6086cd081909f7d4969869b0
   }
 
   access_logs {
@@ -38,10 +32,7 @@ resource "aws_elb" "lb" {
     interval            = 30
   }
 
-<<<<<<< HEAD
   instances                   = ["${element(aws_instance.web.*.id, count.index)}"]
-=======
->>>>>>> f3fa6dd128c29b7b6086cd081909f7d4969869b0
   cross_zone_load_balancing   = true
   idle_timeout                = 400
   connection_draining         = true
